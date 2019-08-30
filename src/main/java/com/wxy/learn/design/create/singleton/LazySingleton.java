21628593d285ef20e0c2cdee904006754286a77e
wangxiayun <wangxiayun@aikucun.com>
2019-08-30 21:31:36 +0800

    工厂方法模式

---
+++ src/main/java/com/wxy/learn/design/create/singleton/LazySingleton.java
@@ -0,0 +1,15 @@
+package com.wxy.learn.design.create.singleton;
+
+
+/**
+ * 懒汉式单例
+ *
+ */
+public class LazySingleton {
+    private static LazySingleton instance = null;
+
+    private LazySingleton(){
+
+    }
+}
+
