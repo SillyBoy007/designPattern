ca05873a54916f33a7b44f69c995c9f36fe8bf1d
wangxiayun <wangxiayun@aikucun.com>
2019-09-26 22:43:14 +0800

    备忘录模式

---
+++ src/main/java/com/wxy/learn/design/action/memento/demo/Memento.java
@@ -0,0 +1,13 @@
+package com.wxy.learn.design.action.memento.demo;
+
+public class Memento {
+    private String state;
+
+    public Memento(String state){
+        this.state = state;
+    }
+
+    public String getState(){
+        return state;
+    }
+}
