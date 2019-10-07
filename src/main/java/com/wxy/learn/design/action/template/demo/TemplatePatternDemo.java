8d5ad0dd765f6cfad62a30e0c9dc936ef3a0ea7d
SillyBoy007 <814752288@qq.com>
2019-10-07 13:23:15 +0800

    模板方法模式

---
+++ src/main/java/com/wxy/learn/design/action/template/demo/TemplatePatternDemo.java
@@ -0,0 +1,12 @@
+package com.wxy.learn.design.action.template.demo;
+
+public class TemplatePatternDemo {
+    public static void main(String[] args) {
+
+        Game game = new Cricket();
+        game.play();
+        System.out.println();
+        game = new Football();
+        game.play();
+    }
+}
